75070748f2a4282efe907bf9b0fc8e1ffe1d0a89
hoanghai145 <hoangthanhhai1308@gmail.com>
2025-08-02 12:44:19 +0700

    Update libs at 2025-08-02 12:44:19

--- Editor/BuildMenu.cs
+++ Editor/BuildMenu.cs
@@ -1,55 +1,53 @@
-namespace BuildHelper.Workflows
+using BuildHelper.Workflows;
+using UnityEditor;
+
+public static class BuildMenu
 {
-    using UnityEditor;
+    [MenuItem("BuildHelper/SetBlueprintPath")]
+    static void SetBlueprintDataPath() { BuildCmd.SetBlueprintDataPath(); }
 
-    public static class BuildMenu
-    {
-        [MenuItem("BuildHelper/SetBlueprintPath")]
-        static void SetBlueprintDataPath() { BuildCmd.SetBlueprintDataPath(); }
+    [MenuItem("BuildHelper/Build Android from Editor")]
+    static void BuildAndroidOnEditor() { BuildCmd.BuildAndroidOnEditor(); }
 
-        [MenuItem("BuildHelper/Build Android from Editor")]
-        static void BuildAndroidOnEditor() { BuildCmd.BuildAndroidOnEditor(); }
+    [MenuItem("BuildHelper/TryRynSyncData")]
+    static void TryRunSyncData() { BuildCmd.TryRunSyncData(); }
 
-        [MenuItem("BuildHelper/TryRynSyncData")]
-        static void TryRunSyncData() { BuildCmd.TryRunSyncData(); }
+    [MenuItem("BuildHelper/Build Android")]
+    static void BuildAndroid() { BuildCmd.BuildAndroid(); }
 
-        [MenuItem("BuildHelper/Build Android")]
-        static void BuildAndroid() { BuildCmd.BuildAndroid(); }
+    [MenuItem("BuildHelper/Build Ios")]
+    static void BuildIos() { BuildCmd.BuildIos(); }
 
-        [MenuItem("BuildHelper/Build Ios")]
-        static void BuildIos() { BuildCmd.BuildIos(); }
+    [MenuItem("BuildHelper/Build WebGl")]
+    static void BuildWebGL() { BuildCmd.BuildWebGL(); }
 
-        [MenuItem("BuildHelper/Build WebGl")]
-        static void BuildWebGL() { BuildCmd.BuildWebGL(); }
+    [MenuItem("BuildHelper/UploadTestFlight")]
+    static void UploadTestFlight() { BuildCmd.UploadTestFlight(); }
 
-        [MenuItem("BuildHelper/UploadTestFlight")]
-        static void UploadTestFlight() { BuildCmd.UploadTestFlight(); }
+    [MenuItem("BuildHelper/UploadAAbToGooglePlay")]
+    static void UploadAAbToGooglePlay() { BuildCmd.UploadAAbToGooglePlay(); }
 
-        [MenuItem("BuildHelper/UploadAAbToGooglePlay")]
-        static void UploadAAbToGooglePlay() { BuildCmd.UploadAAbToGooglePlay(); }
+    [MenuItem("BuildHelper/ProcessBlueprintAndroid")]
+    static void BlueprintWorkFlowAndroid() { BuildCmd.BlueprintWorkFlowAndroid(); }
 
-        [MenuItem("BuildHelper/ProcessBlueprintAndroid")]
-        static void BlueprintWorkFlowAndroid() { BuildCmd.BlueprintWorkFlowAndroid(); }
+    [MenuItem("BuildHelper/ProcessBlueprintIos")]
+    static void BlueprintWorkFlowIos() { BuildCmd.BlueprintWorkFlowIos(); }
 
-        [MenuItem("BuildHelper/ProcessBlueprintIos")]
-        static void BlueprintWorkFlowIos() { BuildCmd.BlueprintWorkFlowIos(); }
+    [MenuItem("BuildHelper/ProcessBlueprintWegbl")]
+    static void BlueprintWorkFlowWebGL() { BuildCmd.BlueprintWorkFlowWebgl(); }
 
-        [MenuItem("BuildHelper/ProcessBlueprintWegbl")]
-        static void BlueprintWorkFlowWebGL() { BuildCmd.BlueprintWorkFlowWebgl(); }
+    [MenuItem("BuildHelper/UploadGoogleAndroid")]
+    static void UploadFile() { UploadBuild.UploadGoogleDriveAndroidPlatform(); }
 
-        [MenuItem("BuildHelper/UploadGoogleAndroid")]
-        static void UploadFile() { UploadBuild.UploadGoogleDriveAndroidPlatform(); }
+    [MenuItem("BuildHelper/UploadGoogleIos")]
+    static void UploadFileIos() { UploadBuild.UploadGoogleDriveIosPlatform(); }
 
-        [MenuItem("BuildHelper/UploadGoogleIos")]
-        static void UploadFileIos() { UploadBuild.UploadGoogleDriveIosPlatform(); }
+    [MenuItem("BuildHelper/UploadGoogleWebGL")]
+    static void UploadFileWebGL() { UploadBuild.UploadGoogleDriveWebGlPlatForm(); }
 
-        [MenuItem("BuildHelper/UploadGoogleWebGL")]
-        static void UploadFileWebGL() { UploadBuild.UploadGoogleDriveWebGlPlatForm(); }
+    [MenuItem("BuildHelper/UploadTelegramWebgl")]
+    static void UploadTelegramWebGL() { UploadBuildTelegram.UploadWebGl(); }
 
-        [MenuItem("BuildHelper/UploadTelegramWebgl")]
-        static void UploadTelegramWebGL() { UploadBuildTelegram.UploadWebGl(); }
-
-        [MenuItem("BuildHelper/Upload CCD")]
-        static void UploadCCD() { UnityCCD.ProcessCcd(); }
-    }
+    [MenuItem("BuildHelper/Upload CCD")]
+    static void UploadCCD() { UnityCCD.ProcessCcd(); }
 }
